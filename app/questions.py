Fixed wrong logic of IDF calculation and added TF-IDF for question set to achive Cosine Similarity

app/questions.py
@@ -1,5 +1,61 @@
+from text.preprocessing import preprocess
+from collections import Counter
+import math
+
 questions = {
-    "qid_1" : "What is your full name?",
-    "qid_2": "Where are you from?",
-    "qid_3": "What do you like to do in your free time?"
+    "qid_1" : "What is your machine learning full name?",
+    "qid_2": "Where are you from machine learning?",
+    "qid_3": "What do you like to do in your full free time?",
+    "qid_4": "What is Machine learning?"
 }
+
+tokens = {}
+
+for qid, question in questions.items():
+    tokens[qid] = preprocess(question)
+
+def questions_bow(tokens):
+    bow = {}
+
+    na = {'token': 0}
+
+    for qid, token in tokens.items():
+        bow[qid] = Counter(token) if token else na
+
+    return dict(bow) 
+
+def tf_idf(tokens):
+    tf = {}
+    df = {}
+    idf = {}
+    tokens_tf_idf = {}
+    doc_count = len(tokens.keys())
+
+    # Compute TF (Locally) =========================================================
+
+    for qid, question in tokens.items():
+        question_length = len(question)
+
+        if qid not in tf:
+            tf[qid] = {}
+
+        for term, freq in question.items():
+            tf[qid][term] = (freq / question_length if question_length else 0)
+
+            df[term] = df.get(term, 0) + 1
+
+    # Compute IDF (Globally) =======================================================
+
+    for term, freq in df.items():
+        idf[term] = math.log10(doc_count / freq)
+
+    for qid in tf.keys():
+
+        if qid not in tokens_tf_idf:
+            tokens_tf_idf[qid] = {}
+
+        for term, freq in tf[qid].items():
+            
+            tokens_tf_idf[qid][term] = round(tf[qid][term] * idf[term], 4)
+
+    return tokens_tf_idf

app/text/feature_extraction.py
@@ -69,10 +69,12 @@ def extract_features_bow(lemma):
     return dict(bow)
 
 def extract_features_tfidf(candidate_id, corpus):
-    doc_count = len(corpus[candidate_id]["bow"].keys())
     tf = {}
-    terms_counts = {}
     idf = {}
+    df = {}
+    doc_overall_count = 0
+
+    # Comput TF (Locally) ===========================================================
 
     for doc in corpus[candidate_id]["bow"].keys():
         total_terms = sum(corpus[candidate_id]["bow"][doc].values())
@@ -84,12 +86,23 @@ def extract_features_tfidf(candidate_id, corpus):
             tf_of_term = term_frequency / total_terms
             tf[doc][term] = tf_of_term
 
-            terms_counts[term] = terms_counts.get(term, 0) + 1
+    # Compute IDF (Globally) ========================================================
 
-    for term, term_frequency in terms_counts.items():
-        idf_score = math.log10(doc_count / term_frequency)
+    for candidate in corpus.values():
+        for doc_tokens in candidate["lemmas"].values():
+            doc_overall_count += 1
 
-        idf[term] = idf_score
+            unique_tokens = set(doc_tokens)
+
+            for token in unique_tokens:
+                df[token] = df.get(token, 0) + 1
+
+    for token, freq in df.items():
+        idf_score = round(math.log10(doc_overall_count / freq), 4)
+
+        idf[token] = idf.get(token, 0) + idf_score
+
+    # Compute TF-IDF ================================================================
 
     for doc in tf.keys():
         for term, term_tf in tf[doc].items():